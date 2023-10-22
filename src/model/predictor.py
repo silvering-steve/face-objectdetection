7aadf4d78435f6e56b809cd8158ef37f83ea592a
silvering-steve <sstevenadi@gmail.com>
2023-10-22 20:51:50 +0700

    updating the apps

--- src/model/predictor.py
+++ src/model/predictor.py
@@ -1,10 +1,18 @@
+import cv2
+import numpy as np
 from ultralytics import YOLO
 
-
-class Model:
+class Predict:
     def __init__(self):
         self.model = YOLO("models/face.pt")
 
+    def __call__(self, image, *args, **kwargs):
+        image_bytes = np.asarray(bytearray(image), dtype=np.uint8)
+        image_array = cv2.imdecode(image_bytes, 1)
+
+        result = self.model.predict(image_array, conf=kwargs['conf'], iou=kwargs['iou'])
+
+        return result[0].plot()
 
 
-model = Model()
+predict = Predict()
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,25 @@
-import cv2
 import streamlit as st
 
-from src.model.predictor import model
-
-hide_img_fs = '''
-    <style>
-    button[title="View fullscreen"]{
-        visibility: hidden;}
-    </style>
-    '''
+from src.model.predictor import predict
 
 
 def main():
     st.set_page_config(page_title="Face Detection")
     st.title("Face Object Detection")
 
-    st.markdown(hide_img_fs, unsafe_allow_html=True)
+    st.sidebar.title("Settings")
+    image = st.sidebar.file_uploader("Face", label_visibility="collapsed")
 
-    cap = cv2.VideoCapture(0)
-    frame_placeholder = st.empty()
+    conf_slider = st.sidebar.slider("Confidence", min_value=0.0, max_value=1.0)
+    iou_slider = st.sidebar.slider("Intersection", min_value=0.0, max_value=1.0)
 
-    while cap.isOpened():
-        ret, frame = cap.read()
+    if st.sidebar.button("Predict"):
+        if image is not None:
+            result = predict(image.read(), conf=conf_slider, iou=iou_slider)
 
-        predicted = model.model.predict(frame)
-        predict_plot = predicted[0].plot()
-
-        frame_placeholder.image(predict_plot, channels="BGR")
-
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+            st.image(result, channels="BGR")
+        else:
+            st.write(":red[Please upload an image first!]")
 
 
 if __name__ == "__main__":
